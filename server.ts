f4f9c52622327ff7b1d11d9e2bc22a0b90876ae4
Ahmad Awais <me@AhmadAwais.com>
2020-05-15 02:31:24 +0500

    📦 NEW: Server

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std/http/server.ts";
 
 const server = serve({ port: 8000 });
 
-console.log("http://localhost:8000/");
+console.log("Running at: http://localhost:8000/");
 
 for await (const req of server) {
   const body = "Hello Deno!";
